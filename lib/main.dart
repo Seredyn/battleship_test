fc2f5185d7db2458a245eeca73490cc848487a82
admin <mr.seredyn@gmail.com>
2024-12-18 19:58:42 +0200

    move pages folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'feature/presentation/bloc/pages/home_page.dart';
+import 'feature/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
